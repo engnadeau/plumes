c27c55262d2610902136beaaafd060e15c19ad79
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-08-21 10:10:10 -0400

    refactor(config): updated variable names and config paths

--- plumes/config.py
+++ plumes/config.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 from dynaconf import Dynaconf
 
-user_settings_path = Path.home() / ".plumes.toml"
-settings_files = [user_settings_path, "settings.toml", ".secrets.toml"]
-settings = Dynaconf(envvar_prefix="PLUMES", settings_files=settings_files)
+user_config_path = Path.home() / ".plumes.toml"
+package_config_path = Path(__file__).parent / "settings.toml"
+settings = Dynaconf(
+    envvar_prefix="PLUMES", settings_files=[user_config_path, package_config_path]
+)
