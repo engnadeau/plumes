a5d037afe13f8995b6c68cea5590be795874c19c
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-09-08 10:09:46 -0400

    test(tests): fixed unused imports/variables and added users to test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,3 @@
-import json
-
 import plumes.cli as pc
 
 
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -6,6 +6,8 @@ def test_set_output(tmp_path):
 
 
 def test_get_user():
-    api = pu.get_api()
     pu.get_user()
     pu.get_user("EngNadeau")
+    pu.get_user("SteveMartinToGo")
+    pu.get_user("alyankovic")
+    pu.get_user("ConanOBrien")
