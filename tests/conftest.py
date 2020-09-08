efb26adf4db7eb9fd5ad1506ad4141ba73419a79
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2020-09-08 10:07:04 -0400

    test(resources): added test resources

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-import pytest
 
+import pytest
 
 RESOURCES_DIR = Path(__file__).parent / "resources"
 
 
 @pytest.fixture
-def friends_path():
-    return RESOURCES_DIR / "test-friends.json"
+def users_path():
+    return RESOURCES_DIR / "test-users.json"
 
 
 @pytest.fixture
